ec9bda16662025ab4d93f786d10199dc1cfb53d5
Service Web <sw@gssmail.com>
2024-03-11 13:41:08 -0500

    Commit changes in C:\Websites\Git\emarlow\4219\GCG_4219_Bedding_Tag.sql

---
+++ GCG_4219_Bedding_Tag.sql
@@ -0,0 +1,12 @@
+/* ==========================================
+ * TABLE: GCG_4219_Bedding_Tag
+ * ========================================== */
+CREATE TABLE "GCG_4219_Bedding_Tag"(
+ "JOB" CHAR(6),
+ "SUFFIX" CHAR(3),
+ "QTY_LAST_PRINTED" BIGINT,
+ "column0" CHAR(6),
+ "column1" CHAR(3),
+ "column2" BIGINT);
+
+
